5694f63a7dc85514cda6f6ec8699c0e84e26162a
dahoon <ekdnj03@gmail.com>
2024-06-03 15:25:39 +0900

    0603

--- front/src/components/BannerSlider.jsx
+++ front/src/components/BannerSlider.jsx
@@ -1,3 +1,4 @@
+import '../css/product.css'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
--- front/src/pages/ProductPage.jsx
+++ front/src/pages/ProductPage.jsx
@@ -1,3 +1,4 @@
+import '../css/product.css'
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from "axios";
@@ -8,6 +9,12 @@ import ListAll from '../components/ListAll';
 
 export default function ProductPage({menu}) {
   const [productlist, setProductList] = useState([])
+  const selectList = [
+    {"value": "popularity", "title": "이름순"},
+    {"value": "highpricestitle","title" : "높은가격순"},
+    {"value": "lospricestitle", "title" : "낮은가격순"}
+    ];
+
   useEffect(() => {
     axios
       .get('/data/product.json')
@@ -20,10 +27,43 @@ export default function ProductPage({menu}) {
     rows.push(productlist.slice(i,i+4))
   }
 
+// 필터
+const handleChange = (e) => {
+  let clickList = e.target.value;
+  let event = [...productlist]
+
+  if(clickList === selectList[0].value){
+    let popularity = event;
+    popularity.sort((a, b)=>{
+      if(a.name > b.name) return 1;
+      if(a.name < b.name) return -1;
+      return 0;
+    })
+    setProductList(popularity);
+  }else if(clickList === selectList[1].value){
+    let highpricestitle = event;
+    highpricestitle.sort((a, b)=>{
+      if(a.price > b.price) return 1;
+      if(a.price < b.price) return -1;
+      return 0;
+    })
+    setProductList(highpricestitle);
+  }else if(clickList === selectList[2].value){
+    let lospricestitle = event;
+    lospricestitle.sort((a, b)=>{
+      if(a.price < b.price) return 1;
+      if(a.price > b.price) return -1;
+      return 0;
+    })
+    setProductList(lospricestitle);
+  }
+
+}
   return (
       <div className='content'>
 			<Location depth1="PRODUCT"/>
 			<SubTitle title="PRODUCT"/>
+      <ListAll eventList={productlist} handleChange={handleChange} selectList={selectList}/>
       {
 				rows.map((row, index)=>(
 					<ul className='ProductPage' key={index}>
@@ -40,29 +80,12 @@ export default function ProductPage({menu}) {
                   <span className='ProductPage-price'>{product.price}</span>
                   <span className="ProductPage-soldout">품절</span>
                 </div>
-
 							</li>
 						))
 					}					
 			</ul>
 				))
 			}
-            {/* <ul className='product_list'>
-              {productlist.map(product => (
-                <li className='product'>
-                  <div className='product_imgbox'>
-                    <Link to={`/detail/${product.id}`}>
-                      <img className='product_img' src={product.image} />
-                    </Link>
-                  </div>
-                  <div className='product_info'>
-                    <span className='product_title'>{product.title}</span>
-                    <span className='product_price'>{product.price}</span>
-                    <span className="product_soldout">품절</span>
-                  </div>
-                </li>
-              ))}
-            </ul> */}
           </div>
   )
 }
