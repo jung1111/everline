5d66c35bc59517d2e8abe96bd0300ddc30c3914e
hong <test@test.com>
2024-06-07 17:28:18 +0900

    hye 0607 notice-main

--- front/src/components/MainNotice.jsx
+++ front/src/components/MainNotice.jsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "../css/board.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 
 export default function MainNotice(){
+		const [noticeData, setNoticeData] = useState([]);
+
+		useEffect(()=>{
+			fetch('data/event.json')
+			.then(res => res.json())
+			.then(result => setNoticeData(result))
+			.catch(error => console.log(error))
+		},[])
+
+		const randomData = () =>{
+			const random = [];
+			const jsonData = [...noticeData];
+
+			for(let i = 0; i < 3; i++ ){
+				const randeomIndex = Math.floor(Math.random * jsonData.length);
+				const randeomData = jsonData.splice(randeomIndex,1)[0];
+				random.push(randeomData)
+				// setNoticeData(random)
+
+			}
+
+			return randomData
+		}
+
+
 		return (
 		<>
 			<h1 className='MainNotice-title'>NEWS & <span className='MainNotice-title-red'>NOTICE</span><span className='moreBtn'>버튼</span></h1>
 			<ul className='MainNotice-list'>
-				<li>
-					
-					<h3>타이틀</h3>
-					<span>날짜</span>
-					<FontAwesomeIcon icon={faChevronRight} />
+			{
+				noticeData.map((item)=>(
+					<li>
+					<div className='MainNotice-link'>						
+						<Link to={'/'}>{item.title}</Link>
+						<span>{item.date}</span>
+					</div>				
+					<i className='MainNotice-icon'/>
 				</li>
+
+				))
+			}
 			</ul>
 		</>
 	);
