e717749fc2391da9b103fea0b7477d9d5cc7e66c
dahoon <ekdnj03@gmail.com>
2024-06-12 14:01:34 +0900

    jeong0612/2

--- front/src/components/BannerSlider.jsx
+++ front/src/components/BannerSlider.jsx
@@ -1,35 +1,34 @@
-
-import '../css/product.css'
+import '../css/product.css';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSwipeable } from 'react-swipeable';
 
 export default function BannerSlider() {
-const [banners, setBanners] = useState([])
+  const [banners, setBanners] = useState([]); // 배너 데이터를 저장할 상태
+  const [currentBanner, setCurrentBanner] = useState(0); // 현재 보여줄 배너의 인덱스를 저장할 상태
 
-useEffect(()=>{
-  axios
-    .get("http://localhost:3000/data/slide.json")
-    .then(res=> setBanners(res.data))
-    .catch(error=>console.log(error));
-},{})
-
-
-const [currentBanner, setCurrentBanner] = useState(0);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/data/slide.json") // 배너 데이터를 가져오는 API 호출
+      .then(res => setBanners(res.data)) // 데이터를 받아 상태에 저장
+      .catch(error => console.log(error)); // 에러 로그 출력
+  }, []);
 
   const goToPrevBanner = () => {
     const prevBanner = (currentBanner + banners.length - 1) % banners.length;
-    setCurrentBanner(prevBanner);
+    setCurrentBanner(prevBanner); // 이전 배너로 이동
   };
 
   const goToNextBanner = () => {
     const nextBanner = (currentBanner + 1) % banners.length;
-    setCurrentBanner(nextBanner);
+    setCurrentBanner(nextBanner); // 다음 배너로 이동
   };
+
   const goToBanner = (index) => {
-    setCurrentBanner(index);
+    setCurrentBanner(index); // 특정 배너로 이동
   };
+
   const handlers = useSwipeable({
     onSwipedLeft: () => goToNextBanner(),
     onSwipedRight: () => goToPrevBanner(),
@@ -38,23 +37,36 @@ const [currentBanner, setCurrentBanner] = useState(0);
   });
 
   return (
-        <div className="BannerSlider-container" >
-          <div className="BannerSlider-content">
-            <div className='currentBanner-left'>{`0${currentBanner+1}`}</div>
-            <div className='currentBanner-right'>{`0${currentBanner+2}`}</div>
-            {banners.map((banner, index)=>(
-              <div {...handlers} className='BannerSlider-imgbox'>
-                <h3 style={{ display: index === currentBanner ? 'block' : 'none' }}><h6 style={{ display: index === currentBanner ? 'block' : 'none' }}>OFFICIAL MD</h6>{banner.title}
-                  <p>{banner.release}</p>
-                  <Link className='link' to="/product" ><button className='more-btn' style={{ display: index === currentBanner ? 'block' : 'none' }}>More &nbsp;&nbsp;‣</button></Link>
-                </h3>
-                <img key={index} src={banner.image} alt={`Banner ${currentBanner + 1}`} style={{ display: index === currentBanner ? 'block' : 'none' }} />
-              </div>
-            ))}
-              <button onClick={goToPrevBanner} className="prev"></button>
-              <button onClick={goToNextBanner} className="next"></button>
-          </div>
-          <div className="pagination">
+    <div className="BannerSlider-container" {...handlers} >
+      <div className="BannerSlider-content">
+        <div className='currentBanner-left'>{`0${currentBanner + 1}`}</div>
+        <div className='currentBanner-right'>{`0${(currentBanner + 1) % banners.length + 1}`}</div>
+        <div className='officialmd-box'><span className='officialmd'>OFFICIAL MD</span></div>
+        <div>
+            <button onClick={goToPrevBanner} className="prev"></button>
+            <button onClick={goToNextBanner} className="next"></button>
+        </div>
+        <div className='BannerSlider-box'>
+          {banners.length > 0 && (
+            <div className='BannerSlider-imgbox'>
+              <img src={banners[currentBanner].image} alt={`Banner ${currentBanner + 1}`} />
+            </div>
+          )}
+          {banners.length > 0 && (
+            <div className='BannerSlider-titlebox'>
+              <h3>
+                <h6>OFFICIAL MD</h6>
+                {banners[currentBanner].title}
+                <p>{banners[currentBanner].release}</p>
+                <Link className='link' to="/product">
+                  <button className='more-btn'>More &nbsp;&nbsp;‣</button>
+                </Link>
+              </h3>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="pagination">
         {banners.map((_, index) => (
           <div
             key={index}
@@ -63,7 +75,6 @@ const [currentBanner, setCurrentBanner] = useState(0);
           ></div>
         ))}
       </div>
-          <span className='officialmd'>OFFICIAL MD</span>
-  </div>
+    </div>
   );
 }
